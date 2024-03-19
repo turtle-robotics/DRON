17e984ff77be782568d8d12b1a07aaae608c6fe8
Ian <ian.wilhite0@gmail.com>
2024-03-19 01:10:22 -0500

    added hotspot detect

---
+++ hotspot_detect.py
@@ -0,0 +1,42 @@
+import cv2 as cv
+import argparse
+
+parser=argparse.ArgumentParser()
+parser.add_argument('--image')
+
+args=parser.parse_args()
+video=cv.VideoCapture(0)
+
+while True:
+    hasFrame,frame=video.read()
+    if not hasFrame:
+        break
+    
+    gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY) 
+    
+    ret, mask = cv.threshold(gray, 100, 255, cv.THRESH_BINARY)
+    print()
+    
+    contours, _ = cv.findContours(mask, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
+    
+    cv.drawContours(frame, contours, -1, (0,255,0), 3)
+    M = cv.moments(mask)
+    if M["m00"] != 0:
+        cx = int(M["m10"] / M["m00"])
+        cy = int(M["m01"] / M["m00"])
+        cv.circle(frame, (cx, cy), 10, (0, 255, 0), thickness=3)
+    
+    print(f'centroid - x:{cx} y:{cy}')
+    
+    
+    center = (frame.shape[1]//2, frame.shape[0]//2)
+    centroid = (cx, cy)
+    cv.line(frame, center, centroid, (0, 0, 255), 3)
+    
+    
+    cv.imshow("blob", frame)
+    #cv.imshow("gray", gray)
+    cv.imshow("thresh", mask)
+
+    if cv.waitKey(1) & 0xFF == ord('q'): 
+        break
