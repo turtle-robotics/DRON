b8d422d90b824eed825f39b6cf4b5601ebd029ee
aayala01 <123396706+aayala01@users.noreply.github.com>
2024-03-26 19:00:40 -0500

    Create ROS2 workspace and added thermal_cam pkg

---
+++ ros2_ws/src/thermal_cam/setup.py
@@ -0,0 +1,25 @@
+from setuptools import find_packages, setup
+
+package_name = 'thermal_cam'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='abel',
+    maintainer_email='abel@todo.todo',
+    description='TODO: Package description',
+    license='MIT',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
