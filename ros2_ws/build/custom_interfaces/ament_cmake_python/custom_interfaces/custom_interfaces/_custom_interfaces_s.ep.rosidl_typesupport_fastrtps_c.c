dfa2c425978f3e9b64e08b5cf45c3d933c4a0557
Brussell03 <r.brussell03@gmail.com>
2024-09-25 18:08:25 -0500

    Add temperature and gps publishers and subscribers

---
+++ ros2_ws/build/custom_interfaces/ament_cmake_python/custom_interfaces/custom_interfaces/_custom_interfaces_s.ep.rosidl_typesupport_fastrtps_c.c
@@ -0,0 +1,171 @@
+// generated from rosidl_generator_py/resource/_idl_pkg_typesupport_entry_point.c.em
+// generated code does not contain a copyright notice
+#include <Python.h>
+
+static PyMethodDef custom_interfaces__methods[] = {
+  {NULL, NULL, 0, NULL}  /* sentinel */
+};
+
+static struct PyModuleDef custom_interfaces__module = {
+  PyModuleDef_HEAD_INIT,
+  "_custom_interfaces_support",
+  "_custom_interfaces_doc",
+  -1,  /* -1 means that the module keeps state in global variables */
+  custom_interfaces__methods,
+  NULL,
+  NULL,
+  NULL,
+  NULL,
+};
+
+#include <stdbool.h>
+#include <stdint.h>
+#include "rosidl_runtime_c/visibility_control.h"
+#include "rosidl_runtime_c/message_type_support_struct.h"
+#include "rosidl_runtime_c/service_type_support_struct.h"
+#include "rosidl_runtime_c/action_type_support_struct.h"
+#include "custom_interfaces/msg/detail/latlong__type_support.h"
+#include "custom_interfaces/msg/detail/latlong__struct.h"
+#include "custom_interfaces/msg/detail/latlong__functions.h"
+
+static void * custom_interfaces__msg__latlong__create_ros_message(void)
+{
+  return custom_interfaces__msg__Latlong__create();
+}
+
+static void custom_interfaces__msg__latlong__destroy_ros_message(void * raw_ros_message)
+{
+  custom_interfaces__msg__Latlong * ros_message = (custom_interfaces__msg__Latlong *)raw_ros_message;
+  custom_interfaces__msg__Latlong__destroy(ros_message);
+}
+
+ROSIDL_GENERATOR_C_IMPORT
+bool custom_interfaces__msg__latlong__convert_from_py(PyObject * _pymsg, void * ros_message);
+ROSIDL_GENERATOR_C_IMPORT
+PyObject * custom_interfaces__msg__latlong__convert_to_py(void * raw_ros_message);
+
+
+ROSIDL_GENERATOR_C_IMPORT
+const rosidl_message_type_support_t *
+ROSIDL_GET_MSG_TYPE_SUPPORT(custom_interfaces, msg, Latlong);
+
+int8_t
+_register_msg_type__msg__latlong(PyObject * pymodule)
+{
+  int8_t err;
+
+  PyObject * pyobject_create_ros_message = NULL;
+  pyobject_create_ros_message = PyCapsule_New(
+    (void *)&custom_interfaces__msg__latlong__create_ros_message,
+    NULL, NULL);
+  if (!pyobject_create_ros_message) {
+    // previously added objects will be removed when the module is destroyed
+    return -1;
+  }
+  err = PyModule_AddObject(
+    pymodule,
+    "create_ros_message_msg__msg__latlong",
+    pyobject_create_ros_message);
+  if (err) {
+    // the created capsule needs to be decremented
+    Py_XDECREF(pyobject_create_ros_message);
+    // previously added objects will be removed when the module is destroyed
+    return err;
+  }
+
+  PyObject * pyobject_destroy_ros_message = NULL;
+  pyobject_destroy_ros_message = PyCapsule_New(
+    (void *)&custom_interfaces__msg__latlong__destroy_ros_message,
+    NULL, NULL);
+  if (!pyobject_destroy_ros_message) {
+    // previously added objects will be removed when the module is destroyed
+    return -1;
+  }
+  err = PyModule_AddObject(
+    pymodule,
+    "destroy_ros_message_msg__msg__latlong",
+    pyobject_destroy_ros_message);
+  if (err) {
+    // the created capsule needs to be decremented
+    Py_XDECREF(pyobject_destroy_ros_message);
+    // previously added objects will be removed when the module is destroyed
+    return err;
+  }
+
+  PyObject * pyobject_convert_from_py = NULL;
+  pyobject_convert_from_py = PyCapsule_New(
+    (void *)&custom_interfaces__msg__latlong__convert_from_py,
+    NULL, NULL);
+  if (!pyobject_convert_from_py) {
+    // previously added objects will be removed when the module is destroyed
+    return -1;
+  }
+  err = PyModule_AddObject(
+    pymodule,
+    "convert_from_py_msg__msg__latlong",
+    pyobject_convert_from_py);
+  if (err) {
+    // the created capsule needs to be decremented
+    Py_XDECREF(pyobject_convert_from_py);
+    // previously added objects will be removed when the module is destroyed
+    return err;
+  }
+
+  PyObject * pyobject_convert_to_py = NULL;
+  pyobject_convert_to_py = PyCapsule_New(
+    (void *)&custom_interfaces__msg__latlong__convert_to_py,
+    NULL, NULL);
+  if (!pyobject_convert_to_py) {
+    // previously added objects will be removed when the module is destroyed
+    return -1;
+  }
+  err = PyModule_AddObject(
+    pymodule,
+    "convert_to_py_msg__msg__latlong",
+    pyobject_convert_to_py);
+  if (err) {
+    // the created capsule needs to be decremented
+    Py_XDECREF(pyobject_convert_to_py);
+    // previously added objects will be removed when the module is destroyed
+    return err;
+  }
+
+  PyObject * pyobject_type_support = NULL;
+  pyobject_type_support = PyCapsule_New(
+    (void *)ROSIDL_GET_MSG_TYPE_SUPPORT(custom_interfaces, msg, Latlong),
+    NULL, NULL);
+  if (!pyobject_type_support) {
+    // previously added objects will be removed when the module is destroyed
+    return -1;
+  }
+  err = PyModule_AddObject(
+    pymodule,
+    "type_support_msg__msg__latlong",
+    pyobject_type_support);
+  if (err) {
+    // the created capsule needs to be decremented
+    Py_XDECREF(pyobject_type_support);
+    // previously added objects will be removed when the module is destroyed
+    return err;
+  }
+  return 0;
+}
+
+PyMODINIT_FUNC
+PyInit_custom_interfaces_s__rosidl_typesupport_fastrtps_c(void)
+{
+  PyObject * pymodule = NULL;
+  pymodule = PyModule_Create(&custom_interfaces__module);
+  if (!pymodule) {
+    return NULL;
+  }
+  int8_t err;
+
+  err = _register_msg_type__msg__latlong(pymodule);
+  if (err) {
+    Py_XDECREF(pymodule);
+    return NULL;
+  }
+
+  return pymodule;
+}
