9d0fcae736a9aa9c45a6b4ca262990a76eea381f
Brussell03 <r.brussell03@gmail.com>
2024-11-21 15:53:05 -0600

    Camera Capturing

--- DRON_Interface/Assets/Scripts/MeshRegionRenderer.cs
+++ DRON_Interface/Assets/Scripts/MeshRegionRenderer.cs
@@ -15,12 +15,26 @@ public class MeshRegionRenderer : MonoBehaviour
     private MeshFilter meshFilter;
     private MeshRenderer meshRenderer;
 
+
+    public Shader paintShader;
+
+    RenderTexture splatMap;
+    Material unpaintedMaterial,paintMaterial;
+
     // Start is called before the first frame update
     void Start()
     {
         maxConcentration = (int)(maxConcentrationPerCubicMeter / (gridSize * gridSize * gridSize));
         meshFilter = GetComponent<MeshFilter>();
         meshRenderer = GetComponent<MeshRenderer>();
+
+        // Mesh Painting Prep
+        paintMaterial = new Material(paintShader);
+        paintMaterial.SetVector("_Color", Color.red);
+
+        unpaintedMaterial = meshRenderer.material;
+        splatMap = new RenderTexture(1024, 1024, 0, RenderTextureFormat.ARGBFloat);
+        unpaintedMaterial.mainTexture = splatMap;
     }
 
     // Update is called once per frame
@@ -99,5 +113,28 @@ public class MeshRegionRenderer : MonoBehaviour
 
         meshFilter.mesh = newMesh;
         meshRenderer.material = mat;
+    }
+
+    public void SplatMesh(Vector3 pos) {
+        // https://stackoverflow.com/questions/60552364/draw-onto-an-objects-texture-based-on-a-raycast-hit-position-in-world-space
+        pos = transform.InverseTransformPoint(pos);
+        
+        paintMaterial.SetVector("_Coordinate", new Vector4(pos.x, pos.y, pos.z, 0));
+        RenderTexture temp = RenderTexture.GetTemporary(splatMap.width, splatMap.height, 0, RenderTextureFormat.ARGBFloat);
+
+        // Cache the old target so that we can reset it later
+        RenderTexture previousRT = RenderTexture.active;
+        RenderTexture.active = temp;
+
+        Material mat = paintMaterial;
+        Mesh mesh = meshFilter.mesh;
+        mat.SetTexture("_MainTex", splatMap);
+        mat.SetPass(0); // This tells the renderer to use pass 0 from this material
+        Graphics.DrawMeshNow(mesh, Vector3.zero, Quaternion.identity);
+
+        RenderTexture.active = previousRT;
+        Graphics.Blit(temp, splatMap);
+
+        RenderTexture.ReleaseTemporary(temp);
     }
 }
---
+++ Stereovision/captureStereoConfigImages.py
@@ -0,0 +1,118 @@
+from ast import Try
+from PyQt5.QtWidgets import QLabel, QHBoxLayout, QVBoxLayout, QApplication, QWidget
+from picamera2 import Picamera2
+from PyQt5.QtGui import QImage,QPixmap
+from PyQt5.QtCore import QThread
+import RPi.GPIO as gp
+import time
+import os
+
+width = 1920
+height = 1080
+
+adapter_info = {
+    "A" : {   
+        "i2c_cmd":"i2cset -y 10 0x70 0x00 0x04",
+        "gpio_sta":[0,0,1],
+    }, "B" : {
+        "i2c_cmd":"i2cset -y 10 0x70 0x00 0x05",
+        "gpio_sta":[1,0,1],
+    }
+}
+
+class WorkThread(QThread):
+
+    def __init__(self):
+        super(WorkThread,self).__init__()
+        gp.setwarnings(False)
+        gp.setmode(gp.BOARD)
+        gp.setup(7, gp.OUT)
+        gp.setup(11, gp.OUT)
+        gp.setup(12, gp.OUT)
+
+
+    def select_channel(self,index):
+        channel_info = adapter_info.get(index)
+        if channel_info == None:
+            print("Can't get this info")
+        gpio_sta = channel_info["gpio_sta"] # gpio write
+        gp.output(7, gpio_sta[0])
+        gp.output(11, gpio_sta[1])
+        gp.output(12, gpio_sta[2])
+
+    def init_i2c(self,index):
+        channel_info = adapter_info.get(index)
+        os.system(channel_info["i2c_cmd"]) # i2c write
+
+    def run(self):
+        global picam2
+        # picam2 = Picamera2()
+        # picam2.configure( picam2.still_configuration(main={"size": (320, 240),"format": "BGR888"},buffer_count=1))
+
+        flag = False
+
+        for item in {"A","B"}:
+            try:
+                self.select_channel(item)
+                self.init_i2c(item)
+                time.sleep(0.5) 
+                if flag == False:
+                    flag = True
+                else :
+                    picam2.close()
+                    # time.sleep(0.5) 
+                print("init1 "+ item)
+                picam2 = Picamera2()
+                picam2.configure(picam2.create_still_configuration(main={"size": (320, 240),"format": "BGR888"},buffer_count=2)) 
+                picam2.start()
+                time.sleep(2)
+                picam2.capture_array(wait=False)
+                time.sleep(0.1)
+            except Exception as e:
+                print("except: "+str(e))
+	
+	i = 0
+        while i < 20:
+            for item in {"A","B"}:
+                self.select_channel(item)
+                time.sleep(4)
+                try:
+                    buf = picam2.capture_array()
+                    buf = picam2.capture_array()
+                    cvimg = QImage(buf, width, height,QImage.Format_RGB888)
+                    cvimg.save(f"stereo_{i}" + item + ".jpg")
+                    pixmap = QPixmap(cvimg)
+                    if item == 'A':
+                        image_label.setPixmap(pixmap)
+                    elif item == 'B':
+                        image_label2.setPixmap(pixmap)
+                except Exception as e:
+                    print("capture_buffer: "+ str(e))
+
+app = QApplication([])
+window = QWidget()
+layout_h = QHBoxLayout()
+layout_v = QVBoxLayout()
+image_label = QLabel()
+image_label2 = QLabel()
+
+# picam2 = Picamera2()
+
+work = WorkThread()
+
+if __name__ == "__main__":
+    image_label.setFixedSize(1920, 1080)
+    image_label2.setFixedSize(1920, 1080)
+    window.setWindowTitle("Qt Picamera2 Arducam Multi Camera Demo")
+    layout_h.addWidget(image_label)    
+    layout_h.addWidget(image_label2)
+    layout_v.addLayout(layout_h,20)
+    window.setLayout(layout_v)
+    window.resize(1920*2, 1080*2)
+
+    work.start()
+
+    window.show()
+    app.exec()
+    work.quit()
+    picam2.close()
